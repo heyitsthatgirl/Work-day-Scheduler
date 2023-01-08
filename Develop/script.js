daabfc989fe63040728d86147f14e922280cce25
hopemansfield <hopemansfield@gmail.com>
2023-01-08 11:44:00 -0500

    time blocks change color according to what time it is

--- Develop/script.js
+++ Develop/script.js
@@ -15,35 +15,45 @@ $(function () {
   })
 
   // display the text from local storage in each schedule block
-  $('#hour-9 .description').val(localStorage.getItem("hour-9"));
-  $('#hour-10 .description').val(localStorage.getItem("hour-10"));
-  $('#hour-11 .description').val(localStorage.getItem("hour-11"));
-  $('#hour-12 .description').val(localStorage.getItem("hour-12"));
-  $('#hour-13 .description').val(localStorage.getItem("hour-13"));
-  $('#hour-14 .description').val(localStorage.getItem("hour-14"));
-  $('#hour-15 .description').val(localStorage.getItem("hour-15"));
-  $('#hour-16 .description').val(localStorage.getItem("hour-16"));
-  $('#hour-17 .description').val(localStorage.getItem("hour-17"));
-  $('#hour-18 .description').val(localStorage.getItem("hour-18"));
+  $('#9 .description').val(localStorage.getItem("9"));
+  $('#10 .description').val(localStorage.getItem("10"));
+  $('#11 .description').val(localStorage.getItem("11"));
+  $('#12 .description').val(localStorage.getItem("12"));
+  $('#13 .description').val(localStorage.getItem("13"));
+  $('#14 .description').val(localStorage.getItem("14"));
+  $('#15 .description').val(localStorage.getItem("15"));
+  $('#16 .description').val(localStorage.getItem("16"));
+  
+// color code the time blocks based on the real time of day
+  function colorCode(){
+    // get the current day and hour from dayjs
+    var currentTime = dayjs().hour();
+    console.log(currentTime);
+    
+    // compare the real time to the integer value of each time block
+    // change the styling using the corresponding css class based on what time it is
+    // remove any classes that should not be applied at that time
+    $('.time-block').each(function() {
+      var timeBlock = parseInt($(this).attr('id'));
+      if(timeBlock < currentTime) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+      }
+      else if(timeBlock === currentTime){
+        $(this).addClass("present");
+        $(this).removeClass("future");
+        $(this).removeClass("past");
+      }
+      else {
+        $(this).addClass("future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+      }
+    })
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  }
+  colorCode();
 });
 
 
